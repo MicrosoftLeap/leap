3a62d78d55102b41d8e44a37ec51ab438d17e665
v-irdown <v-irdown@MININT-F1C7J7A>
2018-01-16 15:49:30 -0800

    commit message goes hear

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,8 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //commment goes here
+            Console.WriteLine("Hello World Friend!");
             Console.WriteLine();
 
             Console.WriteLine("Press any key to exit...");
