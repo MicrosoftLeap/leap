a80133a3d544e753280e6ab88840cf56976b4b0d
samira-ouraga <samiraob71@gmail.com>
2018-01-16 15:50:05 -0800

    bad commit

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World, this is meeeeeeeeeeeee!");
             Console.WriteLine();
 
             Console.WriteLine("Press any key to exit...");
