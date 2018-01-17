a6d4406086e498b526f4cf2ec4471fa68ea51651
v-navalm <v-navalm@MININT-ARGH3O7>
2018-01-16 16:13:19 -0800

    Changed welcome message to hola

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hey-ey-ey-eyey!");
+            Console.WriteLine("Hola!");
             Console.WriteLine();
 
             Console.WriteLine("Press any key to exit...");
