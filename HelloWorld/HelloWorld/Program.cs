e4c678b3108c46847ea6c701e7d53520456aff40
v-dohoop <v-dohoop@MININT-M9L2H7R>
2018-01-16 16:58:09 -0800

    Bananas are delicious

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi Worlds!");
+            Console.WriteLine("Bananas Smell Delicious!");
             Console.WriteLine();
 
             Console.WriteLine("Press any key to exit...");
