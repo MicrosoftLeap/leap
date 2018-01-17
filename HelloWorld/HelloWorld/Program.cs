a81aa8405ef000cff5a4ddb61d6fdc8d9abe8340
v-irdown <v-irdown@MININT-F1C7J7A>
2018-01-16 16:56:59 -0800

    Changes made

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,8 +6,8 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            //commment goes here
-            Console.WriteLine("Hello World Friend!");
+            //commment goes hear
+            Console.WriteLine("Hello World Friend! Fiexed it dood");
             Console.WriteLine();
 
             Console.WriteLine("Press any key to exit...");
