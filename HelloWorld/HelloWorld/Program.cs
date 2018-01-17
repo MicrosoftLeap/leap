2e483e8ea999a2816c2eace7a38adfb7b10bc3bc
Jennifer Feng <jjfeng520@gmail.com>
2018-01-16 16:32:25 -0800

    Updated name to Jen

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!! (this is Jennifer)");
+            Console.WriteLine("Hello World!!!! (this is Jen)");
             Console.WriteLine();
 
             Console.WriteLine("Press any key to exit...");
