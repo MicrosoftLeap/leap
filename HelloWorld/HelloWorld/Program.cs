b2c888e65dc020643d28fa856fdcbfb81ef86dcc
v-dohoop <v-dohoop@MININT-M9L2H7R>
2018-01-16 16:51:28 -0800

    Issue incoming.

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Banana!");
+            Console.WriteLine("Bananas Smell Delicious!");
             Console.WriteLine();
 
             Console.WriteLine("Press any key to exit...");
