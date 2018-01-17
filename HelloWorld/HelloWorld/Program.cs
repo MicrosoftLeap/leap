b7988fb72a0ce434ad6c9ff5dbc5c166c665c46d
chris.stefan1844 <chris.stefan1844@gmail.com>
2018-01-16 16:55:35 -0800

    Changed line 10 in program.cs.

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,8 +6,8 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, You Fool.);
-            Console.WriteLine();
+            Console.WriteLine("Hello, You Fool.");
+            Console.WriteLine("I need more sleep.");
 
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
