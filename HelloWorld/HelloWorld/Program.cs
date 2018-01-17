d4ae3bfcc27cc042c8a804dd8489ee6aac90c522
v-rawil <v-rawil@microsoft.com>
2018-01-16 16:42:39 -0800

    changed to rachel

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -7,7 +7,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello World, this is meeeeeeeeeeeee!");
+            Console.WriteLine("Hello World, this is rachel!");
             Console.WriteLine();
 
             Console.WriteLine("Press any key to exit...");
