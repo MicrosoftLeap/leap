fbeca454fb3e9c6139f1b96403ef20c5d9469713
Adam Yoblick <advolker@microsoft.com>
2018-01-17 10:04:08 -0800

    commit in feature branch

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,10 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine();
-
-            Console.WriteLine("Press any key to exit...");
+            Console.WriteLine("xfththhdttgh");
             Console.ReadKey();
         }
     }
