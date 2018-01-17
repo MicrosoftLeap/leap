3cc8aa5375f4e172b6d1caa371be0f3cd6d85dd2
v-jedug <v-jedug@microsoft.com>
2018-01-16 16:55:55 -0800

    boo

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! DANCE PARTY! and I dont want your changes in my file!");
+            Console.WriteLine("Hello World! DANCE PARTY! and I dont want your changes in my file! BOO BOO");
             Console.WriteLine();
 
             Console.WriteLine("Press any key to exit...");
