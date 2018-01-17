7c35737235f3e7d617cb43db6fd9dc56ead196d4
v-rawil <v-rawil@microsoft.com>
2018-01-16 16:51:38 -0800

    added lastname

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -7,7 +7,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello World, this is rachel!");
+            Console.WriteLine("Hello World, this is rachel williams!");
             Console.WriteLine();
 
             Console.WriteLine("Press any key to exit...");
