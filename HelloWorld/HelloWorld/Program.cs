1f13c33353e32eadec9ddbe6bfe6d430fb6d96f8
v-jejoo <v-jejoo@MININT-HP0VH29>
2018-03-20 11:48:03 -0700

    This is a second commit to the LEAP repo.

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("The answer is 42.");
             Console.WriteLine();
 
             Console.WriteLine("Press any key to exit...");
