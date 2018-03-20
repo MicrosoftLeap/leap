282d6059e17eb3a71fd2461beaff139fc9a6a0c7
Margarita Tagaban <v-mataga@microsoft.com>
2018-03-20 14:41:56 -0700

    hello

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Margie!");
+            Console.WriteLine("Hello Isai!");
             Console.WriteLine();
 
             Console.WriteLine("Press any key to exit...");
