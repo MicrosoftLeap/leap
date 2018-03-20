74b0229c8d5d48ceb6dae5848b10c084b05c4e22
monday ayewa <monday152002@yahoo.com>
2018-03-20 12:03:06 -0700

    testing how to stage and commit in VSTS

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World from Monday Ayewa!");
             Console.WriteLine();
 
             Console.WriteLine("Press any key to exit...");
