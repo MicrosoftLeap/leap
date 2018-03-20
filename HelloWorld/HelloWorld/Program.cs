d6a686fd8d6c37a9916605aea89511069fa02869
Jonathan Martinez <JonM90@gmail.com>
2018-03-20 14:38:16 -0700

    Update Program.cs
    Hello son

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -7,7 +7,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello Washington!");
+            Console.WriteLine("Hello Rogelio!");
             Console.WriteLine();
 
             Console.WriteLine("Press any key to exit...");
