3a0a84fc54e768492d06446bfdedf5995d6538f3
v-askam <ashwini.kamat@gmail.com>
2018-03-20 14:47:32 -0700

    trying merge conflict

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -7,7 +7,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World To All!");//this is a new comment
-            Console.WriteLine();
+            Console.WriteLine();//we have a conflict
 
             Console.WriteLine("Press any key to exit..."); //trying to push now 
             Console.ReadKey();  //changing to update pull request 
