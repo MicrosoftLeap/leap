a2ce9967d582f0d2e6905cfc2fab53cc7362e672
Marty Hernandez Avedon <martyavedon@gmail.com>
2018-03-20 14:28:21 -0700

    deleted text from print statement in line 12

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -9,7 +9,7 @@ namespace HelloWorld
             Console.WriteLine("Merge conflict?!?!");
             Console.WriteLine();
 
-            Console.WriteLine("Press the any key...");
+            Console.WriteLine("");
             Console.ReadKey();
         }
     }
