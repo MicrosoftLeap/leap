cfaa2f3483861c5f00c41242daaecd54d5d5d1ee
v-shiass <v-shiass@MININT-PKSEFRA>
2018-03-20 14:29:00 -0700

    Added a new line

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,8 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello There,this is microsoft!");
+            Console.WriteLine("Hello There,this is microsoft!");
             Console.WriteLine();
 
             Console.WriteLine("Press any key to exit...");
