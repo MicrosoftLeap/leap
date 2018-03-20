2ad66b7a92e102013795885220dc846e89468223
v-jubell <v-jubell@MININT-2J7VQH3>
2018-03-20 14:23:45 -0700

    Change to Program.cs

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Wfrom the eastside of the lake");
             Console.WriteLine();
 
             Console.WriteLine("Press any key to exit...");
