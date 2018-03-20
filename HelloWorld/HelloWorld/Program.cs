1a506d23add99693a05493ff51bd2bb2311f4706
Isai Solis <v-issoli@microsoft.com>
2018-03-20 12:04:29 -0700

    Revert "Commiting staged files."
    This reverts commit a8f8fab5ea65514b3b80502216f74b3b9638423f.

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -7,7 +7,6 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("What's Good?!");
             Console.WriteLine();
 
             Console.WriteLine("Press any key to exit...");
