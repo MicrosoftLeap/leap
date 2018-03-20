73ce20ea8ce4e013cd886e5adfbed1548fccb93e
Shelly Tang (InConsulting) <v-sheta@microsoft.com>
2018-03-20 12:00:49 -0700

    added lunch notice and new txtfile

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -7,6 +7,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("It's almost lunch time!");
             Console.WriteLine();
 
             Console.WriteLine("Press any key to exit...");
