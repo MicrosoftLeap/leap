63b20346635d2658da0addb367ffb515e230219d
v-roschr <rschramm007@gmail.com>
2018-03-20 11:57:08 -0700

    Moved helloworld text into its own seperate class | Added existingFile.txt

---
+++ HelloWorld/HelloWorld/HelloWorld.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class HelloWorld
+    {
+        public HelloWorld()
+        {
+            Console.WriteLine("Hello from sunny seattle! :D");
+            Console.WriteLine();
+
+            Console.WriteLine("Press any key to exit... :D");
+            Console.ReadKey();
+        }
+    }
+}
--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,12 +6,8 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello LEAP World!");
-            Console.WriteLine();
-
-            Console.WriteLine("Press any key to exit... :D");
-            Console.WriteLine("Testing more stuff...");
-            Console.ReadKey();
+            HelloWorld test = new HelloWorld();
+            Console.WriteLine("Testing stuff");
         }
     }
 }
