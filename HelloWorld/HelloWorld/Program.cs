2141887cb71e859bbb782dcf72e5fc335bce0b38
v-rode <v-rode@MININT-O2MFUC4>
2018-03-20 14:51:21 -0700

    Erased Everything

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -7,11 +7,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello Leaper! Keep this line and a ReadKey, delete everything else in this function");
-            Console.WriteLine();
 
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadKey();
         }
     }
 }
