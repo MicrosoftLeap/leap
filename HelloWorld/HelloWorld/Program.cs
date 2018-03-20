55ac0b7d648195785b40daed5966767c74fe41df
Adam Yoblick <advolker@microsoft.com>
2018-03-20 13:34:09 -0700

    change from advolker

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,6 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            dzoijtiosdjtiojt
             Console.WriteLine("Hello from rainy Seattle!");
             Console.WriteLine();
 
