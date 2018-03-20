7dc4eee6c5845b5b411f33321034aa2dd02d9175
Monica Williams <v-mowill@microsoft.com>
2018-03-20 12:02:42 -0700

    This is a commit message, we did some things

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bonjour!");
             Console.WriteLine();
 
             Console.WriteLine("Press any key to exit...");
