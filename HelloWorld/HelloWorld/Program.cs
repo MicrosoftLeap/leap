585f866c121f3027e619f550ab25a6987bd2c932
sadeajayi <v-foajay@microsoft.com>
2018-03-20 11:41:31 -0700

    Modified Program.cs and added a text file

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,8 +6,9 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine();
+            Console.WriteLine("Hello What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hi {0}", name);
 
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
