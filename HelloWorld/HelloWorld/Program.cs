0c8f37ed64528f2a914178124d731e0c36476aa0
Jonathan Martinez <v-jonam@microsoft.com>
2018-03-20 14:54:18 -0700

    Revert "Erased Everything"
    This reverts commit 2141887cb71e859bbb782dcf72e5fc335bce0b38.

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -7,7 +7,11 @@ namespace HelloWorld
         static void Main(string[] args)
         {
 
+            Console.WriteLine("Hello Leaper! Keep this line and a ReadKey, delete everything else in this function");
+            Console.WriteLine();
 
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
     }
 }
