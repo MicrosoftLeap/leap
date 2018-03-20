d3578e2b9372d3ea7d028681ec6c9ea15438e22e
v-karenm <fuentes.karen92@gmail.com>
2018-03-20 13:34:09 -0700

    added last name

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello my name is Karen");
+            Console.WriteLine("Hello my name is Karen fuentes");
             Console.WriteLine();
 
             Console.WriteLine("Press any key to exit...");
