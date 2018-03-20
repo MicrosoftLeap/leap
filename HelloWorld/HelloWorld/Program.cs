049a46ebeb98134ac75143f8442a71430fec25be
sadeajayi <v-foajay@microsoft.com>
2018-03-20 14:34:05 -0700

    added random text on line 9

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-
+            knknknknkkn
             Console.WriteLine("Hello Sadeeeeee!");
             Console.WriteLine();
 
