af6928150904f17f3222972e77f4eb152e6f9eb4
Jonathan Martinez <v-jonam@microsoft.com>
2018-03-20 14:27:21 -0700

    Changed location

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -7,7 +7,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello Seattle!");
+            Console.WriteLine("Hello Washington!");
             Console.WriteLine();
 
             Console.WriteLine("Press any key to exit...");
