d4a8ff50cdd2548f81bc578a1425fcbe3160316e
Sereina Blanchard (InConsulting Inc.) <v-seblan@microsoft.com>
2018-03-20 14:23:44 -0700

    Created merge conflic

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello adam!");
+            Console.WriteLine("Merge conflict?!?!");
             Console.WriteLine();
 
             Console.WriteLine("Press any key to exit...");
