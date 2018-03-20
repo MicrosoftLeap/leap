3ed5cc8027d1f13a2ee818a5b28445a36b8f3cfa
v-stagui <v-stagui@MININT-FR3MQMC>
2018-03-20 14:26:16 -0700

    Added stuff

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -7,6 +7,8 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("hey");
+            Console.WriteLine("he3");
             Console.WriteLine();
 
             Console.WriteLine("Press any key to exit...");
