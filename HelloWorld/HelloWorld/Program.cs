a504cc55a4bac960d557f0ba3649a1fc9912af7d
v-aschin <v-aschin@MININT-19PV3Q4>
2018-03-20 14:22:37 -0700

    Changed as requested by the reviewer

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello from Asawari!");
+            Console.WriteLine("Hello Anitha!");
             Console.WriteLine();
 
             Console.WriteLine("Press any key to exit...");
