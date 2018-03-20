c5a40cc5217b7144132ec0845c2e12d66472618b
ejdelrio <edwinjdelrio@gmail.com>
2018-03-20 14:43:41 -0700

    Resolved yet? IDK :D

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,10 +6,10 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-
             Console.WriteLine("The ginger bread man lives in a ginger bread house.");
             Console.WriteLine("Is he made from the house, or is the house made from him?");
             Console.WriteLine("He scremas, for he does not know.");
+            Console.WriteLine();
 
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
