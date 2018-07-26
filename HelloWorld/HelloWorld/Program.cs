d2406eebf23ac6dd90b14bd5b25d28981eb0b537
Timothy Turner <v-titurn@microsoft.com>
2018-07-26 13:58:53 -0700

    I fixed those things Maggie told me to.

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! This is Tim!");
+            Console.WriteLine("Hello World! This is Tim! This is Tim again!");
             Console.WriteLine();
 
             Console.WriteLine("Press any key to exit...");
