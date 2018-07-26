8a9ac313ca36271fec0a07d457163fcbb5dd93c6
Tabitha Hsia (Inconsulting Inc.) <v-tahsia@microsoft.com>
2018-07-26 14:33:06 -0700

    Revert "I deleted and modified stuff"
    This reverts commit 7486162ac110998edf5d39ab899f8cd9e3c244d2.

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -7,7 +7,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("blah blah blah");
+            Console.WriteLine();
 
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
