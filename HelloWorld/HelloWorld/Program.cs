9d738709b5e651b5c02416bb63dda1963b624292
Tabitha Hsia (Inconsulting Inc.) <v-tahsia@microsoft.com>
2018-07-26 16:42:40 -0700

    i added a meow

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -7,7 +7,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
 
-            Console.WriteLine("HiHIHIHI");
+            Console.WriteLine("meow");
             Console.WriteLine();
 
             Console.WriteLine("Press any key to get your keys, get in your car, commute home, and then make dinner...");
