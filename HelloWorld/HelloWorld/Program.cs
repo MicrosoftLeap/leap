ee0268c2bbb8dd517fe032b98b2f8e617b57f021
Elena Olekh <v-elolek@microsoft.com>
2018-07-26 16:42:58 -0700

    changing text

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -7,7 +7,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
 
-            Console.WriteLine("HiHIHIHI");
+            Console.WriteLine("kuku");
             Console.WriteLine();
 
             Console.WriteLine("Press any key to get your keys, get in your car, commute home, and then make dinner...");
