a5b0c672db9876e120e4798f366be3bbd9009aad
Julia Xu (InConsulting Inc.) <v-julx@microsoft.com>
2018-07-26 13:57:18 -0700

    create text file and modify program.cs

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,10 +6,10 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! Julie was here. WAHAHAHAHAHAHAAHAHAHAHA!");
             Console.WriteLine();
 
-            Console.WriteLine("Press any key to exit...");
+            Console.WriteLine("Press any key to get your keys, get in your car, commute home, and then make dinner...");
             Console.ReadKey();
         }
     }
