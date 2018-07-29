4efb040d8afd5a17cabdc011fe293dd18a4bb148
Vilde Vevatne <v-viveva@microsoft.com>
2018-07-29 12:06:26 -0700

    Edit file

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -7,7 +7,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine();
+            Console.WriteLine("HELLO");
 
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
