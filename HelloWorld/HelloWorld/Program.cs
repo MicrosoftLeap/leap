8399df5dfcf3ff9468c98d212a545323202119e7
v-xiwa15 <v-xiwa15@LEAP-11-03>
2018-11-15 15:32:50 -0800

    create conflicts

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -9,7 +9,7 @@ namespace HelloWorld
             Console.WriteLine("Hello World!");
             Console.WriteLine();
 
-            Console.WriteLine("Press any key to exit...");
+            Console.WriteLine("conflicts...");
             Console.WriteLine("helllooo");
             Console.WriteLine("I'm Eva");
             Console.ReadKey();
