6178f921b24b87fa9337e7b9512b721390e5399b
Kehinde Obanla (Inconsulting Inc.) <v-keoban@microsoft.com>
2018-11-15 11:54:19 -0800

    added my name

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -9,6 +9,9 @@ namespace HelloWorld
             Console.WriteLine("Hello World!");
             Console.WriteLine();
 
+            Console.WriteLine("kehinde");
+            Console.ReadLine();
+
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
         }
