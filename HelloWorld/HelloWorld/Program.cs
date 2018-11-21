c2d8c22c2c8e5dd34ee9be0a14fabb264e3db2aa
Amber Clark <v-amclar@microsoft.com>
2018-11-21 10:17:27 -0800

    Changed a Writeline in Program.cs

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -7,7 +7,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("I'm here!");
+            Console.WriteLine("My favorite Looney Toon character is Daffy Duck.");
             Console.WriteLine();
             Console.WriteLine("Leafffrrrrrrning Git!");
             Console.WriteLine("Press any key to exit...");
