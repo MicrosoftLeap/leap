663715800135ab6b824c87f10ccdf2d8a06da693
Meagan Albright <v-mealbr@microsoft.com>
2018-11-20 22:08:54 -0800

    This commit adds the file testText.txt to the repo and updates Program.cs

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -8,7 +8,7 @@ namespace HelloWorld
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine();
-
+            Console.WriteLine("Learrrrrrning Git!");
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
         }
