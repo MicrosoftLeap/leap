12e88e12004e4d768c07d79daf7cacef3860053c
iamdolores <42007598+iamdolores@users.noreply.github.com>
2018-11-25 23:06:08 -0800

    Git Pull Request Exercise (#122)
    * added empty text file named test.txt for the purpose of this git exercises
    
    * modified this file by adding another console writeline
    
    * change hello world output
    
    * added line and changed hello world statement
    
    * changed "tata" to "Greetings!" in intro

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,8 +6,11 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Greetings!");
+            Console.WriteLine("Hello tiny free World!");
             Console.WriteLine();
+            Console.WriteLine();
+
 
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
